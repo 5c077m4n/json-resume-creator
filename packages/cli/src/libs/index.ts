9eea85734a4f56e836de14a76efddc8cadd25246
Roee Shapira <roy.shap1@gmail.com>
2020-04-09 18:13:15 +0300

    Added remote resume json url type errors.

--- packages/cli/src/libs/index.ts
+++ packages/cli/src/libs/index.ts
@@ -4,23 +4,17 @@ import fetch from 'node-fetch';
 import { Resume } from '../interfaces/resume';
 
 export async function getRemoteJsonResume(url: string): Promise<Resume | undefined> {
-	try {
-		const response = await fetch(url);
-		if (!response.ok) throw Error('There was an error in getting the requested json.');
+	if (typeof url !== 'string' || !url.length) throw Error('The given URL is invalid.');
 
-		const json = await response.json();
-		return json as Resume;
-	} catch (err) {
-		console.error(err);
-	}
+	const response = await fetch(url);
+	if (!response.ok) throw Error('There was an error in getting the requested json.');
+
+	const json = await response.json();
+	return json as Resume;
 }
 
-export async function writeExampleResume(toPath: string): Promise<void> {
-	try {
-		await ensureDir(toPath);
-		const exampleResume: Resume = await readJSON('../assets/resume.json');
-		await writeJSON(toPath, exampleResume);
-	} catch (err) {
-		console.error(err);
-	}
+export async function writeExampleResume(toPath = './'): Promise<void> {
+	await ensureDir(toPath);
+	const exampleResume: Resume = await readJSON('../assets/resume.json');
+	await writeJSON(toPath, exampleResume);
 }
