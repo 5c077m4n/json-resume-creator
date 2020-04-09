e078d876666757d886f5483cf27f281703e07e04
Roee Shapira <roy.shap1@gmail.com>
2020-04-09 18:10:32 +0300

    Added remote resume json getter.

--- packages/cli/src/libs/index.ts
+++ packages/cli/src/libs/index.ts
@@ -1,11 +1,24 @@
 import { readJSON, writeJSON, ensureDir } from 'fs-extra';
+import fetch from 'node-fetch';
 
 import { Resume } from '../interfaces/resume';
 
+export async function getRemoteJsonResume(url: string): Promise<Resume | undefined> {
+	try {
+		const response = await fetch(url);
+		if (!response.ok) throw Error('There was an error in getting the requested json.');
+
+		const json = await response.json();
+		return json as Resume;
+	} catch (err) {
+		console.error(err);
+	}
+}
+
 export async function writeExampleResume(toPath: string): Promise<void> {
 	try {
-		const exampleResume: Resume = await readJSON('../assets/resume.json');
 		await ensureDir(toPath);
+		const exampleResume: Resume = await readJSON('../assets/resume.json');
 		await writeJSON(toPath, exampleResume);
 	} catch (err) {
 		console.error(err);
