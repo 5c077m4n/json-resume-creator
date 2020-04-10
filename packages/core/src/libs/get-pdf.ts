e5283c1438b961cfe78e5c1981b379d476520036
Roee Shapira <roy.shap1@gmail.com>
2020-04-10 20:12:03 +0300

    Improved build scripts.

--- packages/core/src/libs/get-pdf.ts
+++ packages/core/src/libs/get-pdf.ts
@@ -1,12 +1,13 @@
-import puppeteer from 'puppeteer';
+import { launch } from 'puppeteer';
 import { ensureFile } from 'fs-extra';
 
 export async function getPagePdf(filePath: string): Promise<void> {
 	await ensureFile(filePath);
 
-	const browser = await puppeteer.launch();
+	const browser = await launch();
 	const page = await browser.newPage();
 	await page.goto(filePath);
+	await page.waitForNavigation({ waitUntil: 'networkidle2' });
 
 	await page.emulateMediaType('screen');
 	await page.pdf();
