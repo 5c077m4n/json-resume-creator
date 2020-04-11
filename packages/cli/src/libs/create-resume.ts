ef9e50f4aeab50f055676ed2d9cbbfa2ccd3caf0
Roee Shapira <roy.shap1@gmail.com>
2020-04-11 21:11:45 +0300

    Fixed cli typo.

--- packages/cli/src/libs/create-resume.ts
+++ packages/cli/src/libs/create-resume.ts
@@ -2,11 +2,11 @@ import { program } from 'commander';
 import {} from '@json-resume-creator/core';
 
 export async function createResume(): Promise<void> {
-	program
+	await program
 		.option('-t, --theme <type>', 'Choose the theme package that you wish for', 'base')
-		.optin('--html', 'Generate an HTML file of the resume', true)
-		.optin('--pdf', 'Generate a PDF file of the resume', false)
-		.parse(process.argv);
+		.option('--html', 'Generate an HTML file of the resume', true)
+		.option('--pdf', 'Generate a PDF file of the resume', false)
+		.parseAsync(process.argv);
 
 	return;
 }
