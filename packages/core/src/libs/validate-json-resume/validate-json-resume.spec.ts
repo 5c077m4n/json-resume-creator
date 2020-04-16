499ed660eaf3dd9e57733137e8af345e35439828
Roee Shapira <roy.shap1@gmail.com>
2020-04-16 23:10:04 +0300

    Added test.

--- packages/core/src/libs/validate-json-resume/validate-json-resume.spec.ts
+++ packages/core/src/libs/validate-json-resume/validate-json-resume.spec.ts
@@ -20,7 +20,14 @@ describe('Validate Json Resume', () => {
 		expect(isValid).toBeTruthy();
 	});
 
-	test('The schame should be invalid', async () => {
+	test('The schame should be invalid - random json', async () => {
+		const json = { randomField: 123 };
+		const isValid = await validateJsonResume(json);
+
+		expect(isValid).toBeFalsy();
+	});
+
+	test('The schame should be invalid - added field', async () => {
 		const json = await import('../../../assets/resume.json');
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		(json as any).invalidField = true;
