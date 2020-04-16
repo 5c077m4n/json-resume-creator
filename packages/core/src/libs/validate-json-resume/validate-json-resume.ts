668aba4f9345194671ea0ef485a4bcb508379b99
Roee Shapira <roy.shap1@gmail.com>
2020-04-16 23:37:53 +0300

    Minor edits.

--- packages/core/src/libs/validate-json-resume/validate-json-resume.ts
+++ packages/core/src/libs/validate-json-resume/validate-json-resume.ts
@@ -1,12 +1,10 @@
 import { Validator } from 'jsonschema';
 
-import * as resumeSchema from '../../schemas/resume.schema.json';
+import { definitions as schemas } from '../../schemas/resume.schema.json';
 
 const resumeValidator = new Validator();
 export async function validateJsonResume(json: unknown): Promise<boolean> {
-	const {
-		definitions: { Resume },
-	} = resumeSchema;
+	const { Resume } = schemas;
 	const res = resumeValidator.validate(json, Resume);
 	return res.valid;
 }
